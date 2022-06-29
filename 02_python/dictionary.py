00bd26e14e91103849d42b87bd6f40e80e8cb682
victornworgu <victornworgu@gmail.com>
2022-06-29 20:23:00 +0100

    added a shorter syntax

--- 02_python/dictionary.py
+++ 02_python/dictionary.py
@@ -1,4 +1,8 @@
 family = {"Victor": "Husband to Vivian, and dad to Starr and Reign.", "Vivian": "Wife to Victor, and mum to Starr and reign.", "Starr": "First child, daughter, and sister to Reign.", "Reign": "Second and last child, son, and brother to Starr."}
-q = input("Search for: ")
+
+"""
+q = input("Enter Keyword: ")
 print(family[q])
-print(family)
+"""
+
+print(family[input("Enter Keyword: ")])
