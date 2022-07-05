c80a3223d201ec2bafd71b1180e7f5c0ac141385
victornworgu <victornworgu@gmail.com>
2022-07-05 14:34:15 +0100

    import functions implementation of multiplication tabe

---
+++ 02_python/multiply.py
@@ -0,0 +1,5 @@
+#from multitable import multiply #import just function from "multitable.py" file. See Method 1 call code below.
+import multitable #import whole "multitable.py" file. See option 2 call code below.
+
+#multiply(int(input("Enter number to multiply: "))) # Method 1
+multitable.multiply(int(input("Enter number to multiply: "))) #Method 2
---
+++ 02_python/multitable.py
@@ -0,0 +1,3 @@
+def multiply (x):
+	for y in range (1,13):
+		print(f"{x} x {y} = {x*y}")
